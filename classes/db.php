457695fa9258e03d89081d496f8e37b7d847b978
houssamlambara <lambarahoussam@gmail.com>
2025-01-03 14:56:12 +0100

    update db.php

--- classes/db.php
+++ classes/db.php
@@ -18,5 +18,16 @@
         public function getConnection() {
             return $this->pdo;
         }
+        public function connect() {
+            if ($this->pdo === null) {
+                try {
+                    $this->pdo = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+                    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch (PDOException $e) {
+                    die("Connection failed: " . $e->getMessage());
+                }
+            }
+            return $this->pdo;
+        }
     }
     ?>
